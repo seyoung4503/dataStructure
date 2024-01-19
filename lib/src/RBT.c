4749d3874dacd6577add34830a5356cbadda3bf2
최세영 <seyoung4503@gmail.com>
2024-01-19 19:11:34 +0900

    add Red Black Tree

--- lib/src/RBT.c
+++ lib/src/RBT.c
@@ -1,16 +1,24 @@
 
 #include "RBT.h"
 
-RBTNode* Nil;
+extern RBTNode* Nil;
 
 void RBT_DestroyTree(RBTNode* Tree)
 {
+    if (Tree->Left != Nil)
+        RBT_DestroyTree(Tree->Left);
+    if (Tree->Right != Nil)
+        RBT_DestroyTree(Tree->Right);
+    
+    Tree->Left = Nil;
+    Tree->Right = Nil;
 
+    RBT_DestroyNode(Tree);
 }
 
 RBTNode* RBT_CreateNode(ElementType NewData)
 {
-    RBTNode* NewNode = (RBTNode*)malloc(sizeof(RBTNode));
+    RBTNode* NewNode = (RBTNode*)malloc( sizeof(RBTNode) );
     NewNode->Parent = NULL;
     NewNode->Left = NULL;
     NewNode->Right = NULL;
@@ -34,6 +42,16 @@ RBTNode* RBT_SearchNode(RBTNode * Tree, ElementType Target)
     else if (Tree->Data < Target)
         return RBT_SearchNode(Tree->Right, Target);
     else    
+        return Tree;
+}
+
+RBTNode* RBT_SearchMinNode(RBTNode* Tree)
+{
+    if (Tree == Nil)
+        return Nil;
+    if (Tree->Left != Nil)
+        return RBT_SearchMinNode(Tree->Left);
+    else
         return Tree;
 }
 
@@ -63,6 +81,61 @@ void RBT_InsertNodeHelper(RBTNode** Tree, RBTNode* NewNode)
     }
 }
 
+void RBT_RebuildAfterInsert(RBTNode** Root, RBTNode* X)
+{
+
+    while (X != (*Root) && X->Parent->Color == RED)
+    {
+        if (X->Parent == X->Parent->Parent->Left)
+        {
+            RBTNode* Uncle = X->Parent->Parent->Right;
+            if (Uncle->Color == RED)
+            {
+                Uncle->Color = BLACK;
+                X->Parent->Parent->Color = RED;
+                X->Parent->Color = BLACK;
+                X = X->Parent->Parent;
+            }
+            else 
+            {
+                if (X == X->Parent->Right)  // Uncle->Color == 'BLACK' && X == X->Parent->Right
+                {
+                    X = X->Parent;
+                    RBT_RotateLeft(Root, X);
+                }
+                // Uncle->Color == 'BLACK' && X == X->Parent->Left
+                X->Parent->Color = BLACK;
+                X->Parent->Parent->Color = RED;
+                RBT_RotateRight(Root, X->Parent->Parent);
+            }
+        }
+        else
+        {
+            RBTNode* Uncle = X->Parent->Parent->Left;
+            if (Uncle->Color == RED)
+            {
+                X->Parent->Color = BLACK;
+                Uncle->Color = BLACK;
+                X->Parent->Parent->Color = RED;
+                X = X->Parent->Parent;
+            }
+            else
+            {
+                if (X == X->Parent->Left)
+                {
+                    X = X->Parent;
+                    RBT_RotateRight(Root, X);
+                }
+
+                X->Parent->Color = BLACK;
+                X->Parent->Parent->Color = RED;
+                RBT_RotateLeft(Root, X->Parent->Parent);
+            }
+        }
+    }
+    (*Root)->Color = BLACK;
+}
+
 void RBT_InsertNode(RBTNode** Tree, RBTNode* NewNode)
 {
     RBT_InsertNodeHelper(Tree, NewNode);
@@ -72,3 +145,212 @@ void RBT_InsertNode(RBTNode** Tree, RBTNode* NewNode)
     RBT_RebuildAfterInsert(Tree, NewNode);
 }
 
+void RBT_RotateRight(RBTNode** Root, RBTNode* Parent)
+{
+    RBTNode* LeftChild = Parent->Left;
+    Parent->Left = LeftChild->Right;
+
+    if (LeftChild->Right != Nil)
+        LeftChild->Right->Parent = Parent;
+
+    LeftChild->Parent = Parent->Parent;
+
+    if (Parent->Parent == NULL)
+        (*Root) = LeftChild;
+    else 
+    {
+        if (Parent == Parent->Parent->Left)
+            Parent->Parent->Left = LeftChild;
+        else
+            Parent->Parent->Right = LeftChild;
+    }
+
+    LeftChild->Right = Parent;
+    Parent->Parent = LeftChild;
+
+}
+
+void RBT_RotateLeft(RBTNode** Root, RBTNode* Parent)
+{
+    RBTNode* RightChild = Parent->Right;
+    Parent->Right = RightChild->Left;
+
+    if (RightChild->Left != Nil)
+        RightChild->Left->Parent = Parent;
+
+    RightChild->Parent = Parent->Parent;
+
+    if (Parent->Parent == NULL)
+        (*Root) = RightChild;
+    else
+    {
+        if (Parent == Parent->Parent->Left)
+            Parent->Parent->Left = RightChild;
+        else
+            Parent->Parent->Right = RightChild;
+    }
+
+    RightChild->Left = Parent;
+    Parent->Parent = RightChild;
+}
+
+void RBT_RebuildAfterRemove(RBTNode** Root, RBTNode* Successor)
+{   
+    RBTNode* Sibling = NULL;
+    while (Successor->Parent != NULL && Successor->Color == BLACK)
+    {
+        if (Successor == Successor->Parent->Left)
+        {   
+            Sibling = Successor->Parent->Right;
+            // no.1 p.262
+            if (Sibling->Color == RED)
+            {
+                Successor->Parent->Color = RED;
+                Sibling->Color = BLACK;
+                RBT_RotateLeft(Root, Successor->Parent);
+            }
+            else 
+            {
+                // 2-A. p.263
+                if (Sibling->Left->Color == BLACK && Sibling->Right->Color == BLACK)
+                {
+                    Sibling->Color = RED;
+                    Successor = Successor->Parent;
+                    // Successor의 Color가 RED일 경우 BLACK으로 수정(while 이후)
+                    // Successor->Color가 원래 검은 색인 경우 이중 흑색은 어떻게 처리?
+                }
+                // 2-B p.264 형제 자식이 R, B 일 경우 if 문 조건 안에서 B, R으로 변함
+                else
+                {
+                    if (Sibling->Left->Color == RED)
+                    {
+                        Sibling->Color = RED;
+                        Sibling->Left->Color = BLACK;
+                        RBT_RotateRight(Root, Sibling);
+                        Sibling = Successor->Parent->Right;
+                    }
+                    // 2-C p.266 // 조건 명시 안한 이유는 2-B 조건에서 넘어오는 경우가 있음
+                    Sibling->Color = Successor->Parent->Color;
+                    Successor->Parent->Color = BLACK;
+                    Sibling->Right->Color = BLACK;
+                    RBT_RotateLeft(Root, Successor->Parent);
+                    Successor = (*Root);
+                }
+            }
+        }
+        else // Left, Right 반전
+        {
+            Sibling = Successor->Parent->Left;
+            // no.1 p.262
+            if (Sibling->Color == RED)
+            {
+                Successor->Parent->Color = RED;
+                Sibling->Color = BLACK;
+                RBT_RotateRight(Root, Successor->Parent);
+            }
+            else 
+            {
+                // 2-A.
+                if (Sibling->Left->Color == BLACK && Sibling->Right->Color == BLACK)
+                {
+                    Sibling->Color = RED;
+                    Successor = Successor->Parent;
+                }
+                // 2-B 
+                if (Sibling->Right->Color == RED)
+                {
+                    Sibling->Color = RED;
+                    Sibling->Right->Color = BLACK;
+                    RBT_RotateLeft(Root, Sibling);
+                    Sibling = Successor->Parent->Left;
+                }
+                // 2-C p.266 // 조건 명시 안한 이유는 2-B 조건에서 넘어오는 경우가 있음
+                Sibling->Color = Successor->Parent->Color;
+                Successor->Parent->Color = BLACK;
+                Sibling->Left->Color = BLACK;
+                RBT_RotateRight(Root, Successor->Parent);
+                Successor = (*Root);
+            }
+        }
+    }
+    Successor->Color = BLACK;
+}
+
+RBTNode* RBT_RemoveNode(RBTNode** Root, ElementType Data)
+{
+    RBTNode* Removed = NULL;
+    RBTNode* Successor = NULL;
+    RBTNode* Target = RBT_SearchNode((*Root), Data);
+
+    if (Target == NULL)
+        return NULL;
+    
+    if (Target->Left == Nil || Target->Right == Nil)
+        Removed = Target;
+
+    else
+    {
+        Removed = RBT_SearchMinNode(Target->Right);
+        Target->Data = Removed->Data;
+    }
+
+    if (Removed->Left == Nil)
+        Successor = Removed->Right;
+    else
+        Successor = Removed->Left;
+
+    Successor->Parent = Removed->Parent;
+
+    if (Removed->Parent == NULL)
+        (*Root) = Successor;
+    else
+    {
+        if (Removed == Removed->Parent->Left)
+            Removed->Parent->Left = Successor;
+        else
+            Removed->Parent->Right = Successor;
+    }
+
+    if (Removed->Color == BLACK)
+        RBT_RebuildAfterRemove(Root, Successor);
+
+    return Removed;
+}
+
+void RBT_PrintTree(RBTNode* Node, int Depth, int BlackCount)
+{
+    int i = 0;
+    char c = 'X';
+    int v = -1;
+    char cnt[100];
+
+    if (Node == NULL || Node == Nil)
+        return;
+
+    if (Node->Color == BLACK)
+        BlackCount ++;
+    
+    if (Node ->Parent != NULL)
+    {
+        v = Node->Parent->Data;
+
+        if (Node->Parent->Left == Node)
+            c = 'L';
+        else
+            c = 'R';
+    }
+
+    if (Node->Left == Nil && Node->Right == Nil)
+        sprintf(cnt, "-------- %d", BlackCount);
+    else
+        sprintf(cnt, "");
+    
+    for(i = 0; i < Depth; i++) 
+        printf(" ");
+    
+    printf( "%d %s [%c, %d] %s\n", Node->Data, (Node->Color == RED)?"RED":"BLACK", c, v, cnt);
+
+    RBT_PrintTree(Node->Left, Depth+1, BlackCount);
+    RBT_PrintTree(Node->Right, Depth+1, BlackCount);
+
+}
--- main.c
+++ main.c
@@ -1,154 +1,238 @@
-#include "DLL.h"
-#include "ArrayStack.h"
-#include "CircularQueue.h"
-#include "LCRS.h"
-#include "BinaryTree.h"
+// #include "DLL.h"
+// #include "ArrayStack.h"
+// #include "CircularQueue.h"
+// #include "LCRS.h"
+// #include "BinaryTree.h"
+#include "RBT.h"
+
+RBTNode* Nil;
 
 int main()
 {
-    // DLL ex
-    Node * List = NULL;
-    Node * NewNode = NULL;
+    // // DLL ex
+    // Node * List = NULL;
+    // Node * NewNode = NULL;
     
-    NewNode = DLL_CreateNode(117);
+    // NewNode = DLL_CreateNode(117);
     
-    DLL_AppendNode(&List, NewNode);
-    //ListPrint(List);
+    // DLL_AppendNode(&List, NewNode);
+    // //ListPrint(List);
 
-    NewNode = DLL_CreateNode(119);
-    DLL_AppendNode(&List, NewNode);
-    ListPrint(List);
+    // NewNode = DLL_CreateNode(119);
+    // DLL_AppendNode(&List, NewNode);
+    // ListPrint(List);
 
 
-    Node * List1 = NULL;
-    NewNode = DLL_CreateNode(120);
+    // Node * List1 = NULL;
+    // NewNode = DLL_CreateNode(120);
     
-    DLL_AppendNode(&List1, NewNode);
-    //ListPrint(List);
+    // DLL_AppendNode(&List1, NewNode);
+    // //ListPrint(List);
 
-    NewNode = DLL_CreateNode(121);
-    DLL_AppendNode(&List1, NewNode);
-    ListPrint(List1);
+    // NewNode = DLL_CreateNode(121);
+    // DLL_AppendNode(&List1, NewNode);
+    // ListPrint(List1);
 
-    // Stack ex
-    int i = 0; 
-    ArrayStack* Stack = NULL;
-    AS_CreateStack(&Stack, 10);
-    AS_Push(Stack, 3);
-    AS_Push(Stack, 37);
-    AS_Push(Stack, 11);
-    AS_Push(Stack, 12);
+    // // Stack ex
+    // int i = 0; 
+    // ArrayStack* Stack = NULL;
+    // AS_CreateStack(&Stack, 10);
+    // AS_Push(Stack, 3);
+    // AS_Push(Stack, 37);
+    // AS_Push(Stack, 11);
+    // AS_Push(Stack, 12);
 
-    printf("%d, %d, %d\n\n", Stack->Capacity, AS_GetSize(Stack), AS_Top(Stack));
+    // printf("%d, %d, %d\n\n", Stack->Capacity, AS_GetSize(Stack), AS_Top(Stack));
 
-    for (i = 0; i < 4; i++)
-    {
-        if (AS_IsEmpty(Stack))
-            break;
+    // for (i = 0; i < 4; i++)
+    // {
+    //     if (AS_IsEmpty(Stack))
+    //         break;
         
-        printf("pop! %d, ", AS_Pop(Stack));
+    //     printf("pop! %d, ", AS_Pop(Stack));
 
-        if (!AS_IsEmpty(Stack))
-            printf("Cur Top : %d\n", AS_Top(Stack));
-        else
-            printf("Stack Empty!\n");
-    }
+    //     if (!AS_IsEmpty(Stack))
+    //         printf("Cur Top : %d\n", AS_Top(Stack));
+    //     else
+    //         printf("Stack Empty!\n");
+    // }
 
-    AS_DestroyStack(Stack);
+    // AS_DestroyStack(Stack);
 
-    // Queue ex
-    i = 0;
-    CircularQueue* Queue;
+    // // Queue ex
+    // i = 0;
+    // CircularQueue* Queue;
 
-    CQ_CreateQueue(&Queue, 10);
+    // CQ_CreateQueue(&Queue, 10);
 
-    CQ_EnQueue(Queue, 1);
-    CQ_EnQueue(Queue, 2);
-    CQ_EnQueue(Queue, 3);
-    CQ_EnQueue(Queue, 4);
+    // CQ_EnQueue(Queue, 1);
+    // CQ_EnQueue(Queue, 2);
+    // CQ_EnQueue(Queue, 3);
+    // CQ_EnQueue(Queue, 4);
 
-    for (i = 0; i < 3; i++)
+    // for (i = 0; i < 3; i++)
+    // {
+    //     printf("Dequeue %d, ", CQ_DeQueue(Queue));
+    //     printf("Front %d, Rear %d\n", Queue->Front, Queue->Rear);
+    // }
+
+    // i = 100;
+    // while (CQ_IsFull(Queue) == 0)
+    // {
+    //     CQ_EnQueue(Queue, i++);
+    // }
+
+    // printf("%d, %d\n\n", Queue->Capacity, CQ_GetSize(Queue));
+    // while (CQ_IsEmpty(Queue) == 0)
+    // {
+    //     printf("Dequeue %d, ", CQ_DeQueue(Queue));
+    //     printf("Front %d, Rear %d\n", Queue->Front, Queue->Rear);
+    // }
+
+    // CQ_DestroyQueue(Queue);
+
+
+    // // // LCRS ex
+    // // LCRSNode* Root = LCRS_CreateNode('A');
+
+    // // LCRSNode* B = LCRS_CreateNode('B');
+    // // LCRSNode* C = LCRS_CreateNode('C');
+    // // LCRSNode* D = LCRS_CreateNode('D');
+    // // LCRSNode* E = LCRS_CreateNode('E');
+    // // LCRSNode* F = LCRS_CreateNode('F');
+    // // LCRSNode* G = LCRS_CreateNode('G');
+    // // LCRSNode* H = LCRS_CreateNode('H');
+    // // LCRSNode* I = LCRS_CreateNode('I');
+    // // LCRSNode* J = LCRS_CreateNode('J');
+    // // LCRSNode* K = LCRS_CreateNode('K');
+
+    // // LCRS_Add_ChildNode(Root, B);
+    // // LCRS_Add_ChildNode(B, C);
+    // // LCRS_Add_ChildNode(B, D);
+    // // LCRS_Add_ChildNode(D, E);
+    // // LCRS_Add_ChildNode(D, F);
+
+    // // LCRS_Add_ChildNode(Root, G);
+    // // LCRS_Add_ChildNode(G, H);
+
+    // // LCRS_Add_ChildNode(Root, I);
+    // // LCRS_Add_ChildNode(I, J);
+    // // LCRS_Add_ChildNode(J, K);
+
+    // // LCRS_PrintTree(Root, 0);
+
+    // // LCRS_DestroyTree(Root);
+
+    // SBTNode* A = SBT_CreateNode('A');
+    // SBTNode* B = SBT_CreateNode('B');
+    // SBTNode* C = SBT_CreateNode('C');
+    // SBTNode* D = SBT_CreateNode('D');
+    // SBTNode* E = SBT_CreateNode('E');
+    // SBTNode* F = SBT_CreateNode('F');
+    // SBTNode* G = SBT_CreateNode('G');
+
+    // A->Left = B;
+    // B->Left = C;
+    // B->Right = D;
+
+    // A->Right = E;
+    // E->Left = F;
+    // E->Right = G;
+
+    // printf("Preorder ...\n");
+    // SBT_PreorderPrintTree(A);
+    // printf("\n\n");
+
+    // printf("Inorder ...\n");
+    // SBT_InorderPrintTree(A);
+    // printf("\n\n");
+
+    // printf("Postorder ...\n");
+    // SBT_PostorderPrintTree(A);
+    // printf("\n\n");
+
+    // SBT_DestroyTree(A);
+
+
+    // RBT ex
+    RBTNode* Tree = NULL;
+    RBTNode* Node = NULL;
+
+    Nil = RBT_CreateNode(0);
+    Nil->Color = BLACK;
+
+    while (1)
     {
-        printf("Dequeue %d, ", CQ_DeQueue(Queue));
-        printf("Front %d, Rear %d\n", Queue->Front, Queue->Rear);
+        int cmd = 0;
+        int param = 0;
+        char buffer[10];
+
+        RBTNode* debug = NULL;
+
+        printf("Enter command number : \n");
+        printf("(1) Create a node, (2) Remove a node, (3) Search a node, (4) Display Tree, (5) Quit\n");
+        printf("Command number : ");
+
+        fgets(buffer, sizeof(buffer)-1, stdin);
+        sscanf(buffer, "%d", &cmd);
+
+        if (cmd < 1 || cmd > 5)
+        {
+            printf("Invalid command number.\n");
+            continue;
+        }
+        else if (cmd == 4)
+        {
+            RBT_PrintTree(Tree, 0, 0);
+            printf("\n");
+            continue;
+        }
+        else if (cmd == 5)
+            break;
+
+        printf("Enter param (1~200)\n");
+
+        fgets(buffer, sizeof(buffer)-1, stdin);
+        sscanf(buffer, "%d", &param);
+
+        if (param < 1 || param > 200)
+        {
+            printf("Invalid param. %d\n", param);
+            continue;
+        }
+
+        switch (cmd)
+        {
+            case 1:
+                // debug = RBT_CreateNode(param);
+                // printf("debug %d", debug->Data);
+                RBT_InsertNode(&Tree, RBT_CreateNode(param));
+                break;
+
+            case 2:
+                Node = RBT_RemoveNode(&Tree, param);
+
+                if (Node == NULL)
+                    printf("NOT FOUND, %d", param);
+                else
+                    RBT_DestroyNode(Node);
+                
+                break;
+            
+            case 3:
+                Node = RBT_SearchNode(Tree, param);
+
+                if (Node == NULL)
+                    printf("NOT FOUND, %d", param);
+                else
+                    printf( "FOUND NODE : %d(Color:%s)\n", Node->Data, (Node->Color == RED)?"RED":"BLACK");
+                
+                break;
+            
+            printf("\n");
+
+        }
     }
-
-    i = 100;
-    while (CQ_IsFull(Queue) == 0)
-    {
-        CQ_EnQueue(Queue, i++);
-    }
-
-    printf("%d, %d\n\n", Queue->Capacity, CQ_GetSize(Queue));
-    while (CQ_IsEmpty(Queue) == 0)
-    {
-        printf("Dequeue %d, ", CQ_DeQueue(Queue));
-        printf("Front %d, Rear %d\n", Queue->Front, Queue->Rear);
-    }
-
-    CQ_DestroyQueue(Queue);
-
-
-    // // LCRS ex
-    // LCRSNode* Root = LCRS_CreateNode('A');
-
-    // LCRSNode* B = LCRS_CreateNode('B');
-    // LCRSNode* C = LCRS_CreateNode('C');
-    // LCRSNode* D = LCRS_CreateNode('D');
-    // LCRSNode* E = LCRS_CreateNode('E');
-    // LCRSNode* F = LCRS_CreateNode('F');
-    // LCRSNode* G = LCRS_CreateNode('G');
-    // LCRSNode* H = LCRS_CreateNode('H');
-    // LCRSNode* I = LCRS_CreateNode('I');
-    // LCRSNode* J = LCRS_CreateNode('J');
-    // LCRSNode* K = LCRS_CreateNode('K');
-
-    // LCRS_Add_ChildNode(Root, B);
-    // LCRS_Add_ChildNode(B, C);
-    // LCRS_Add_ChildNode(B, D);
-    // LCRS_Add_ChildNode(D, E);
-    // LCRS_Add_ChildNode(D, F);
-
-    // LCRS_Add_ChildNode(Root, G);
-    // LCRS_Add_ChildNode(G, H);
-
-    // LCRS_Add_ChildNode(Root, I);
-    // LCRS_Add_ChildNode(I, J);
-    // LCRS_Add_ChildNode(J, K);
-
-    // LCRS_PrintTree(Root, 0);
-
-    // LCRS_DestroyTree(Root);
-
-    SBTNode* A = SBT_CreateNode('A');
-    SBTNode* B = SBT_CreateNode('B');
-    SBTNode* C = SBT_CreateNode('C');
-    SBTNode* D = SBT_CreateNode('D');
-    SBTNode* E = SBT_CreateNode('E');
-    SBTNode* F = SBT_CreateNode('F');
-    SBTNode* G = SBT_CreateNode('G');
-
-    A->Left = B;
-    B->Left = C;
-    B->Right = D;
-
-    A->Right = E;
-    E->Left = F;
-    E->Right = G;
-
-    printf("Preorder ...\n");
-    SBT_PreorderPrintTree(A);
-    printf("\n\n");
-
-    printf("Inorder ...\n");
-    SBT_InorderPrintTree(A);
-    printf("\n\n");
-
-    printf("Postorder ...\n");
-    SBT_PostorderPrintTree(A);
-    printf("\n\n");
-
-    SBT_DestroyTree(A);
-
+    RBT_DestroyTree(Tree);
     return 0;
 }
